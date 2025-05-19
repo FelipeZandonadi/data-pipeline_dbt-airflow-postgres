b2099dbe70ea118fc03b180d7b0216f4e175770b
Felipe Zandonadi <felipe.santos@db1.com.br>
2025-05-19 18:38:30 -0300

    Modeling new dim_date and dim_product_category

--- dags/dbt/models/gld_data/dim_date.sql
+++ dags/dbt/models/gld_data/dim_date.sql
@@ -2,40 +2,24 @@
     materialized='table',
     alias='dim_date',
     post_hook=[
-        'ALTER TABLE gld_data.dim_date ADD PRIMARY KEY (date_key)'
+        'ALTER TABLE gld_data.dim_date ADD PRIMARY KEY (id_date_sk)',
     ]
 ) }}
 
 WITH
     dates AS (
         SELECT
-            extract(YEAR FROM sto.order_purchase_timestamp) AS year_num,
-            extract(MONTH FROM sto.order_purchase_timestamp) AS month_num,
-            extract(DAY FROM sto.order_purchase_timestamp) AS day_num,
-            extract(ISODOW FROM sto.order_purchase_timestamp) AS dow_num,
-            extract(QUARTER FROM sto.order_purchase_timestamp) AS quarter_num
-        FROM slv_data.slv_tb_orders AS sto
-        WHERE sto.order_purchase_timestamp IS NOT NULL
-        UNION
-        SELECT
-            extract(YEAR FROM sto.order_approved_at),
-            extract(MONTH FROM sto.order_approved_at),
-            extract(DAY FROM sto.order_approved_at),
-            extract(ISODOW FROM sto.order_approved_at),
-            extract(QUARTER FROM sto.order_approved_at)
+            CAST(sto.order_approved_at AS DATE) AS date,
+            extract(YEAR FROM sto.order_approved_at) AS year,
+            extract(MONTH FROM sto.order_approved_at) AS month,
+            extract(DAY FROM sto.order_approved_at) AS day,
+            extract(ISODOW FROM sto.order_approved_at) AS day_of_week,
+            extract(QUARTER FROM sto.order_approved_at) AS quarter
         FROM slv_data.slv_tb_orders AS sto
         WHERE sto.order_approved_at IS NOT NULL
         UNION
         SELECT
-            extract(YEAR FROM sto.order_delivered_carrier_date),
-            extract(MONTH FROM sto.order_delivered_carrier_date),
-            extract(DAY FROM sto.order_delivered_carrier_date),
-            extract(ISODOW FROM sto.order_delivered_carrier_date),
-            extract(QUARTER FROM sto.order_delivered_carrier_date)
-        FROM slv_data.slv_tb_orders AS sto
-        WHERE sto.order_delivered_carrier_date IS NOT NULL
-        UNION
-        SELECT
+            CAST(sto.order_delivered_customer_date AS DATE),
             extract(YEAR FROM sto.order_delivered_customer_date),
             extract(MONTH FROM sto.order_delivered_customer_date),
             extract(DAY FROM sto.order_delivered_customer_date),
@@ -45,6 +29,7 @@ WITH
         WHERE sto.order_delivered_customer_date IS NOT NULL
         UNION
         SELECT
+            CAST(sto.order_estimated_delivery_date AS DATE),
             extract(YEAR FROM sto.order_estimated_delivery_date),
             extract(MONTH FROM sto.order_estimated_delivery_date),
             extract(DAY FROM sto.order_estimated_delivery_date),
@@ -55,36 +40,28 @@ WITH
     )
 
 SELECT
-    CAST(d.year_num AS text) ||
+    CAST(d.year AS text) ||
     CASE
-        WHEN length(CAST(d.month_num AS text)) = 1 then '0' || CAST(d.month_num AS text)
-        ELSE CAST(d.month_num AS text)
+        WHEN length(CAST(d.month AS text)) = 1 then '0' || CAST(d.month AS text)
+        ELSE CAST(d.month AS text)
         END ||
     CASE
-        WHEN length(CAST(d.day_num AS text)) = 1 THEN '0' || CAST(d.day_num AS text)
-        ELSE CAST(d.day_num AS text)
-        END AS date_key,
-    CAST(d.year_num AS text) || '-' ||
-    CASE
-        WHEN length(CAST(d.month_num AS text)) = 1 then '0' || CAST(d.month_num AS text)
-        ELSE CAST(d.month_num AS text)
-        END || '-' ||
-    CASE
-        WHEN length(CAST(d.day_num AS text)) = 1 THEN '0' || CAST(d.day_num AS text)
-        ELSE CAST(d.day_num AS text)
-        END AS date_text,
-    d.year_num AS year_num,
-    d.month_num AS month_num,
-    d.day_num AS day_num,
+        WHEN length(CAST(d.day AS text)) = 1 THEN '0' || CAST(d.day AS text)
+        ELSE CAST(d.day AS text)
+        END AS id_date_sk,
+    d.date AS date,
+    d.year AS year,
+    d.month AS month,
+    d.day AS day,
     case
-        when d.dow_num = 1 then 'monday'
-        when d.dow_num = 2 then 'tuesday'
-        when d.dow_num = 3 then 'wednesday'
-        when d.dow_num = 4 then 'thursday'
-        when d.dow_num = 5 then 'friday'
-        when d.dow_num = 6 then 'saturday'
-        when d.dow_num = 7 then 'sunday'
+        when d.day_of_week = 1 then 'monday'
+        when d.day_of_week = 2 then 'tuesday'
+        when d.day_of_week = 3 then 'wednesday'
+        when d.day_of_week = 4 then 'thursday'
+        when d.day_of_week = 5 then 'friday'
+        when d.day_of_week = 6 then 'saturday'
+        when d.day_of_week = 7 then 'sunday'
         else 'unknown'
-    end AS day_week,
-    d.quarter_num AS quarter
+    end AS day_of_week,
+    d.quarter AS quarter
 FROM dates d
--- dags/dbt/models/gld_data/dim_product_category.sql
+++ dags/dbt/models/gld_data/dim_product_category.sql
@@ -2,7 +2,7 @@
     materialized='table',
     alias='dim_product_category',
     post_hook=[
-        'ALTER TABLE gld_data.dim_product_category ADD PRIMARY KEY (product_category_key)'
+        'ALTER TABLE gld_data.dim_product_category ADD PRIMARY KEY (id_product_category_sk)'
     ]
 )}}
 
@@ -11,6 +11,6 @@ WITH
         SELECT DISTINCT coalesce(stp.product_category_name, 'unknown') product_category_name
         FROM {{ source('slv_data', 'slv_tb_products') }} stp
     )
-SELECT md5(pc.product_category_name) product_category_key,
+SELECT md5(pc.product_category_name) id_product_category_sk,
        pc.product_category_name
 FROM product_category pc
